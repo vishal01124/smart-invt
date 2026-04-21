70369325f4dfc136c80b827c77289f439329e00a
vishal01124 <vvasita7@gmail.com>
2026-04-21 13:44:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-// ============================================================
+const SUPABASE_URL = "https://mwerulfadajqwiynpcdw.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13ZXJ1bGZhZGFqcXdpeW5wY2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NDkxNjAsImV4cCI6MjA5MjMyNTE2MH0.i4k0Uz_IRR3Yk6ycp-RkwkvsGXPxNtWjNdQM1pJ7I_4";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);// ============================================================
 //  PharmaDist Pro — Frontend App (Server-Connected)
 //  API Backend: http://localhost:5000/api
 // ============================================================
